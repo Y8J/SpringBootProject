e49689e9d957a8a2f74f856f4854bc287496939b
unknown <2055176329@qq.com>
2018-06-26 10:09:10 +0800

    上传SpringBootSSM整合项目

---
+++ SpringBootSSM/src/main/java/gd/com/SpringBootSsmApplication.java
@@ -0,0 +1,17 @@
+package gd.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主函数入口
+ * @author yangjing
+ *
+ */
+@SpringBootApplication
+public class SpringBootSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSsmApplication.class, args);
+	}
+}
